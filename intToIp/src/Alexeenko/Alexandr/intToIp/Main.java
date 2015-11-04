e739121d953223d152808e51c300574188abb3e8
alex_jd <twoalexan@mail.ru>
2015-11-05 02:40:47 +0500

    new class intToIp

--- intToIp/src/Alexeenko/Alexandr/intToIp/Main.java
+++ intToIp/src/Alexeenko/Alexandr/intToIp/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String Ip = "192.168.1.1";
+        IpToInt numIp = new IpToInt(Ip);
+
+        System.out.print(numIp.aton(Ip) + "\n");
+
+
+        IntToIp IP = new IntToIp(numIp.aton(Ip));
+
+        System.out.print(IP.ntoa(numIp.aton(Ip)));
     }
 }
