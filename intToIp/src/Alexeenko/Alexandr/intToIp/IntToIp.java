8504bf7072bcce99cc4da65f9452af2a4362d5be
alex_jd <twoalexan@mail.ru>
2015-11-06 01:00:03 +0500

    доделал

--- intToIp/src/Alexeenko/Alexandr/intToIp/IntToIp.java
+++ intToIp/src/Alexeenko/Alexandr/intToIp/IntToIp.java
@@ -11,19 +11,24 @@ public class IntToIp {
         this.number = number;
     }
 
+
+
     public String ntoa (long number) {
-        long temp;
-        for (int i = 1; i <= 4; i++ ) {
-            temp = number % 256;
-            if (Ip == null) {
-                Ip = temp + "";
-            } else {
-                Ip = temp + "." + Ip;
+        if (number == -1) {
+            Ip = "255.255.255.255";
+        } else {
+            long temp;
+            for (int i = 1; i <= 4; i++) {
+                temp = number % 256;
+                if (Ip == null) {
+                    Ip = temp + "";
+                } else {
+                    Ip = temp + "." + Ip;
+                }
+                number = number / 256;
             }
-
-            number = number/256;
-
         }
         return Ip;
     }
+
 }
--- intToIp/src/Alexeenko/Alexandr/intToIp/Main.java
+++ intToIp/src/Alexeenko/Alexandr/intToIp/Main.java
@@ -1,17 +1,30 @@
 package Alexeenko.Alexandr.intToIp;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String Ip = "192.168.1.1";
-        IpToInt numIp = new IpToInt(Ip);
+        String str = "Enter the Ip number (integer Ip) : ";
+        System.out.print(str);
 
-        System.out.print(numIp.aton(Ip) + "\n");
+        Scanner input = new Scanner(System.in);
+        String IP_addr = input.nextLine();
 
 
-        IntToIp IP = new IntToIp(numIp.aton(Ip));
+        //String Ip = IP_addr;
+        //IpToInt numIp = new IpToInt(Ip);
+        //System.out.print(numIp.aton(Ip) + "\n");
 
-        System.out.print(IP.ntoa(numIp.aton(Ip)));
+        //IntToIp IP = new IntToIp(numIp.aton(Ip));
+        //System.out.print(IP.ntoa(numIp.aton(Ip)) + "\n");
+
+        Long number = Long.parseLong(IP_addr);
+        IntToIp IP = new IntToIp(number);
+        System.out.print(IP.ntoa(number) + "\n");
+
+        //IntToIp mask_32 = new IntToIp(-1);
+        //System.out.print(mask_32.ntoa(-1) + "\n");
     }
 }
