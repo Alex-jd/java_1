6dbf11b77ee966142a6075e583c927a2ad6b5fbe
alex_jd <twoalexan@mail.ru>
2015-11-20 23:39:09 +0500

    massiv

---
+++ massiv/src/com/company/Array.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * Created by Alex_JD on 14.11.2015.
+ */
+public class Array {
+    int[] internalArray;
+
+    Array () {
+
+    }
+
+    public void calcArray (int[] internalArray) {
+        System.out.print("Что-то делаем с массивом");
+    }
+}
